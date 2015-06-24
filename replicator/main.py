dc34ba8bed58a60e94ca6f5951a91c9324c10230
Jean-Loup Adde <jean-loup.adde@masternaut.com>
2015-06-24 14:08:44 +0200

    Add delete function to account table.

--- replicator/main.py
+++ replicator/main.py
@@ -1,6 +1,5 @@
 from configparser import ConfigParser
 import json
-from datetime import datetime
 from elasticsearch import Elasticsearch
 from pymysqlreplication import BinLogStreamReader
 from pymysqlreplication.row_event import (
@@ -30,7 +29,9 @@ def main():
                                 server_id=3,
                                 only_events=[DeleteRowsEvent, UpdateRowsEvent, WriteRowsEvent],
                                 blocking=True)
-    # TODO Not reexecute action already found
+    print("Connected to the database at %s:%d with user %s" % (MYSQL_SETTINGS.get("host"),
+                                                               MYSQL_SETTINGS.get("port"),
+                                                               MYSQL_SETTINGS.get("user")))
     es_stream = Elasticsearch()
     for binlogevent in stream:
         for row in binlogevent.rows:
@@ -38,15 +39,22 @@ def main():
             if isinstance(binlogevent, DeleteRowsEvent):
                 event["action"] = "delete"
                 event = dict(list(event.items()) + list(row["values"].items()))
+                document_id_to_remove = row["values"]["Id"]
+                doc = es_stream.delete(index="fleet", doc_type="account", id=document_id_to_remove)
+                print("Deleted document for id %d" % document_id_to_remove)
             elif isinstance(binlogevent, UpdateRowsEvent):
                 event["action"] = "update"
                 event = dict(list(event.items()) + list(row["after_values"].items()))
+                document_id_to_update = row["before_values"]["Id"]
+                updated_body = row["after_values"]
+                doc = es_stream.index(index="fleet", doc_type="account", id=document_id_to_update, body=updated_body)
+                print("Document for id %d updated to %s" % (document_id_to_update, row["after_values"]))
             elif isinstance(binlogevent, WriteRowsEvent):
-
                 event["action"] = "insert"
                 event = dict(list(event.items()) + list(row["values"].items()))
+                document_id_to_add = row["values"]["Id"]
                 print(row['values'])
-                doc = es_stream.index(index="fleet", doc_type="account", body=row["values"], timestamp=datetime.now())
+                doc = es_stream.index(index="fleet", doc_type="account", body=row["values"], id=document_id_to_add)
                 print("Adding ", doc)
             print(json.dumps(event))
             sys.stdout.flush()
