94c21aabeb471f71163f3cda4b5f9be83ad48d21
Jean-Loup Adde <jean-loup.adde@masternaut.com>
2015-06-29 14:59:13 +0200

    Using log_pos instead of timestamp.

--- replicator/replicator.py
+++ replicator/replicator.py
@@ -5,7 +5,6 @@ import sys
 
 from elasticsearch import Elasticsearch
 from pymysqlreplication import BinLogStreamReader
-from pymysqlreplication.event import XidEvent
 
 from pymysqlreplication.row_event import (
     DeleteRowsEvent,
@@ -33,72 +32,65 @@ class Replicator:
         self.server_id = parser.getint('mysql', 'server_id')
         self.transaction_manager = TransactionManager()
         self.modules_manager = ModulesManager(config_parser=parser)
+        self.index_label = parser.get('mysql', 'index_label')
 
     def start(self):
         # server_id is your slave identifier, it should be unique.
         # set blocking to True if you want to block and wait for the next event at
         # the end of the stream
 
-        if self.transaction_manager.get_last_request_sent_timestamp() == '':
+        if hasattr(self.transaction_manager, 'last_request_sent'):
+            stream = BinLogStreamReader(connection_settings=self.MYSQL_SETTINGS,
+                             only_events=[DeleteRowsEvent, UpdateRowsEvent, WriteRowsEvent],
+                             server_id=self.server_id,
+                             only_tables=["account"],
+                             blocking=True,
+                             resume_stream=True,
+                             log_pos=self.transaction_manager.last_request_sent)
+        else:
             stream = BinLogStreamReader(connection_settings=self.MYSQL_SETTINGS, server_id=self.server_id,
                                          only_tables=["account"],
                                          only_events=[DeleteRowsEvent, UpdateRowsEvent, WriteRowsEvent],
                                          blocking=True)
-        else:
-            timestamp = self.transaction_manager.get_last_request_sent_timestamp() + 1
-            stream = BinLogStreamReader(connection_settings=self.MYSQL_SETTINGS,
-                                         only_events=[DeleteRowsEvent, UpdateRowsEvent, WriteRowsEvent, XidEvent],
-                                         server_id=self.server_id,
-                                         only_tables=["account"],
-                                         blocking=True,
-                                         skip_to_timestamp=timestamp)
+
 
         self.logger.info("Connected to the database at %s:%d with user %s" % (self.MYSQL_SETTINGS.get("host"),
                                                                    self.MYSQL_SETTINGS.get("port"),
                                                                    self.MYSQL_SETTINGS.get("user")))
         for binlogevent in stream:
-            if isinstance(binlogevent, XidEvent):
-                self.logger.info("Xid Event detected")
-            else:
-                for row in binlogevent.rows:
-                    event = {"schema": binlogevent.schema, "table": binlogevent.table}
-                    if isinstance(binlogevent, DeleteRowsEvent):
-                        self.logger.info("Deletion event detected.")
-                        event["action"] = "delete"
-                        event = dict(list(event.items()) + list(row["values"].items()))
-                        document_id_to_remove = row["values"]["Id"]
-                        self.transaction_manager.last_request_sent_timestamp = binlogevent.timestamp
-                        self.transaction_manager.write_last_request_sent(binlogevent)
-                        self.modules_manager.remove_data_all_modules(index=binlogevent.schema, doc_type=binlogevent.table, id=document_id_to_remove)
-                        self.transaction_manager.write_last_request_success(binlogevent)
-                        self.transaction_manager.last_success_timestamp = binlogevent.timestamp
+            for row in binlogevent.rows:
+                event = {"schema": binlogevent.schema, "table": binlogevent.table}
+                if isinstance(binlogevent, DeleteRowsEvent):
+                    self.logger.info("Deletion event detected.")
+                    event["action"] = "delete"
+                    event = dict(list(event.items()) + list(row["values"].items()))
+                    document_id_to_remove = row["values"][self.index_label]
+                    self.transaction_manager.write_last_request_log_pos(stream, binlogevent)
+                    self.modules_manager.remove_data_all_modules(index=binlogevent.schema, doc_type=binlogevent.table, id=document_id_to_remove)
+                    self.transaction_manager.write_last_success_log_pos(stream, binlogevent)
 
-                        self.logger.info("Deleted document for id %d" % document_id_to_remove)
-                    elif isinstance(binlogevent, UpdateRowsEvent):
-                        self.logger.info("Updated event detected.")
-                        event["action"] = "update"
-                        event = dict(list(event.items()) + list(row["after_values"].items()))
-                        document_id_to_update = row["before_values"]["Id"]
-                        updated_body = row["after_values"]
-                        self.transaction_manager.last_request_sent_timestamp = binlogevent.timestamp
-                        self.transaction_manager.write_last_request_sent(binlogevent)
-                        self.modules_manager.update_data_all_modules(index=binlogevent.schema, doc_type=binlogevent.table, id=document_id_to_update, doc=updated_body)
-                        self.transaction_manager.write_last_request_success(binlogevent)
-                        self.transaction_manager.last_success_timestamp = binlogevent.timestamp
-                        self.logger.info("Document for id %d updated to %s" % (document_id_to_update, row["after_values"]))
-                    elif isinstance(binlogevent, WriteRowsEvent):
-                        self.logger.info("Insert event detected.")
-                        event["action"] = "insert"
-                        event = dict(list(event.items()) + list(row["values"].items()))
-                        document_id_to_add = row["values"]["Id"]
-                        self.transaction_manager.last_request_sent_timestamp = binlogevent.timestamp
-                        self.transaction_manager.write_last_request_sent(binlogevent)
-                        self.modules_manager.insert_data_all_modules(index=binlogevent.schema, doc_type=binlogevent.table, doc=row["values"], id=document_id_to_add)
-                        self.transaction_manager.write_last_request_success(binlogevent)
-                        self.transaction_manager.last_success_timestamp = binlogevent.timestamp
-                        self.logger.info("Adding document %s to the elastic search" % row["values"])
+                    self.logger.info("Deleted document for id %d" % document_id_to_remove)
+                elif isinstance(binlogevent, UpdateRowsEvent):
+                    self.logger.info("Updated event detected.")
+                    event["action"] = "update"
+                    event = dict(list(event.items()) + list(row["after_values"].items()))
+                    document_id_to_update = row["before_values"][self.index_label]
+                    updated_body = row["after_values"]
+                    self.transaction_manager.write_last_request_log_pos(stream, binlogevent)
+                    self.modules_manager.update_data_all_modules(index=binlogevent.schema, doc_type=binlogevent.table, id=document_id_to_update, doc=updated_body)
+                    self.transaction_manager.write_last_success_log_pos(stream, binlogevent)
+                    self.logger.info("Document for id %d updated to %s" % (document_id_to_update, row["after_values"]))
+                elif isinstance(binlogevent, WriteRowsEvent):
+                    self.logger.info("Insert event detected.")
+                    event["action"] = "insert"
+                    event = dict(list(event.items()) + list(row["values"].items()))
+                    document_id_to_add = row["values"][self.index_label]
+                    self.transaction_manager.write_last_request_log_pos(stream, binlogevent)
+                    self.modules_manager.insert_data_all_modules(index=binlogevent.schema, doc_type=binlogevent.table, doc=row["values"], id=document_id_to_add)
+                    self.transaction_manager.write_last_success_log_pos(stream, binlogevent)
+                    self.logger.info("Adding document %s to the elastic search" % row["values"])
     #                self.logger.info(json.dumps(event))
-                sys.stdout.flush()
+            sys.stdout.flush()
 
 if __name__ == "__main__":
     replicator = Replicator()
--- replicator/transaction_manager.py
+++ replicator/transaction_manager.py
@@ -3,9 +3,14 @@ class TransactionManager:
     def __init__(self):
         self._last_success_file = open('resources/transaction/last_success.tmp', 'r+')
         self._last_request_file = open('resources/transaction/last_request.tmp', 'r+')
-        self.last_success_timestamp = int(self._last_success_file.readline())
-        self.last_request_sent_timestamp = int(self._last_request_file.readline())
-
+        line = self._last_success_file.readline()
+        if line != "":
+            self.last_success = int(line)
+        line = self._last_request_file.readline()
+        if line != "":
+            self.last_request_sent = int(line)
+        self.last_success_timestamp = 0
+        self.last_request_sent_timestamp = 0
         self.number_of_create_request = 0
         self.number_of_delete_request = 0
         self.number_of_update_request = 0
@@ -14,32 +19,29 @@ class TransactionManager:
     def number_of_transactions(self):
         return self.number_of_create_request + self.number_of_delete_request + self.number_of_update_request
 
-    def get_last_success_timestamp(self):
-        return self.last_success_timestamp
-
-    def get_last_request_sent_timestamp(self):
-        return self.last_request_sent_timestamp
-
     @property
     def latency(self):
         return self.last_success_timestamp - self.last_request_sent_timestamp
 
     @property
     def is_synchronized(self):
-        return self.get_last_request_sent_timestamp() == self.get_last_success_timestamp()
-
-    def write_last_request_success(self, log_event):
-        timestamp = self.__get_timestamp(log_event)
-        self._last_success_file.seek(0)
-        self._last_success_file.write('%d' % timestamp)
-
-    def write_last_request_sent(self, log_event):
-        timestamp = self.__get_timestamp(log_event)
-        self._last_request_file.seek(0)
-        self._last_request_file.write('%d' % timestamp)
+        return self.last_request_sent_timestamp == self.last_success_timestamp
 
     def __get_timestamp(self, log_event):
         return log_event.timestamp
+
+    def write_last_request_log_pos(self, stream, log_event):
+        log_pos = stream.log_pos
+        self._last_request_file.seek(0)
+        self._last_request_file.write('%d' % log_pos)
+        self.last_request_sent_timestamp = self.__get_timestamp(log_event)
+
+    def write_last_success_log_pos(self, stream, log_event):
+        log_pos = stream.log_pos
+        self._last_success_file.seek(0)
+        self._last_success_file.write('%d' % log_pos)
+        self.last_success_timestamp = log_event.timestamp
+
 
     def __del__(self):
         self._last_request_file.close()
