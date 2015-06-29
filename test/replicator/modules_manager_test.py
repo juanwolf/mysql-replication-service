ec5694e38af70bf75ded6be3ae3b69282b16fc58
Jean-Loup Adde <jean-loup.adde@masternaut.com>
2015-06-29 16:20:02 +0200

    Add test to modules_manager.

--- test/replicator/modules_manager_test.py
+++ test/replicator/modules_manager_test.py
@@ -1,18 +1,25 @@
 from configparser import ConfigParser
 import unittest
-from modules_manager import ModulesDetector
-
+from modules.elasticsearch_module import ElasticsearchModule
+from modules_manager import ModulesManager
 
 class ModuleDetectorTest(unittest.TestCase):
 
     def setUp(self):
         config_parser = ConfigParser()
-        config_parser.read('resources/conf/config.ini')
-        self.module_detector = ModulesDetector(config_parser)
+        config_parser.read('test/resources/config.ini')
+        self.modules_manager = ModulesManager(config_parser)
 
 
-    def test_modules_detector_should_find_the_elasticsearch_module(self):
+    def test_modules_manager_should_find_the_elasticsearch_module(self):
         # given
         # when
         # then
-        self.assertIn('elasticsearch', self.module_detector.modules_available)
+        self.assertIn('elasticsearch', self.modules_manager.modules_available)
+
+    def test_modules_manager_should_create_instance_of_es_module(self):
+        # given
+        # when
+        self.modules_manager.generate_modules_instances()
+        # then
+        self.assertIsInstance(self.modules_manager.instances[0], ElasticsearchModule)
