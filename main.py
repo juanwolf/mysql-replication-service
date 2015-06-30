788b6c05eacd1044f48872d833542b1bd26c92e2
Jean-Loup Adde <jean-loup.adde@masternaut.com>
2015-06-30 17:01:39 +0200

    Add logging and the config file can configure the server (host, port, logs)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#!/bin/python3
 from concurrent.futures import ThreadPoolExecutor
 from configparser import ConfigParser
+import logging
 import cherrypy
 import click
 from replicator.replicator import Replicator
@@ -19,7 +21,21 @@ def main(server):
 
     executor = ThreadPoolExecutor(max_workers = 2)
     executor.submit(replicator.start)
+
+    logger = logging.getLogger('replicator')
     if server:
+        if config_parser.has_section('server') :
+            server_config = {
+                'server.socket_host' : config_parser.get('server', 'socket_host'),
+                'server.socket_port' : config_parser.getint('server', 'socket_port'),
+                'log.access_file' : config_parser.get('server', 'log.access_file'),
+                'log.error_file' : config_parser.get('server', 'log.error_file')
+            }
+            cherrypy.config.update(server_config)
+            logger.info('Server will be started at %s:%d' % (server_config['server.socket_host'],
+                                                             server_config['server.socket_port']))
+        else:
+            logger.info('No settings found, using the default one. (localhost:8080)')
         executor.submit(cherrypy.quickstart(ReplicatorServer(replicator)))
 
 
