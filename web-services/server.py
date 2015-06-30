fc4c8ced09cd64324ceb89cfca02820c8829e8b9
Jean-Loup Adde <jean-loup.adde@masternaut.com>
2015-06-30 14:38:31 +0200

    Using JSON instead of returning an html containing the json (berk).

--- web-services/server.py
+++ web-services/server.py
@@ -7,19 +7,19 @@ from replicator.replicator import Replicator
 
 class ReplicatorServer(object):
 
-    def __init__(self):
-        self.replicator = Replicator()
-        self.thread_service = Thread(None, self.replicator.start(), 'thread_service')
+    def __init__(self, replicator):
+        self.replicator = replicator
 
     @cherrypy.expose
+    @cherrypy.tools.json_out()
     def index(self):
-        return json.dumps({
-            "transaction_numberNumber" : self.replicator.transaction_manager.number_of_transactions,
-            "creation_number" :  self.replicator.transaction_manager.number_of_create_request,
-            "deletion_number" : self.replicator.transaction_manager.number_of_delete_request,
-            "update_number" : self.replicator.transaction_manager.number_of_update_request,
-            "latency" : self.replicator.transaction_manager.latency,
-        })
+        return {
+            'transaction_numberNumber' : self.replicator.transaction_manager.number_of_transactions,
+            'creation_number' :  self.replicator.transaction_manager.number_of_create_request,
+            'deletion_number' : self.replicator.transaction_manager.number_of_delete_request,
+            'update_number' : self.replicator.transaction_manager.number_of_update_request,
+            'latency' : self.replicator.transaction_manager.latency,
+        }
 
 
 
