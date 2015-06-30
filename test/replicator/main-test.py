4f6826c893b382a854634868f3e643a9b1318266
Jean-Loup Adde <jean-loup.adde@masternaut.com>
2015-06-30 11:21:52 +0200

    Add tests to check filtering for databases.

--- test/replicator/main-test.py
+++ test/replicator/main-test.py
@@ -15,6 +15,7 @@ class ReplicatorTest(TestCase):
             'user' : 'user',
             'password' : 'password',
             'server_id' : '1',
+            'databases' : 'db',
             'tables' : 'account',
             'index_label' : 'id'
         }
@@ -36,6 +37,7 @@ class ReplicatorTest(TestCase):
             'port' : '1111',
             'user' : 'user',
             'password' : 'password',
+            'databases' : 'db',
             'server_id' : '1',
             'tables' : 'account, testbase',
             'index_label' : 'id'
@@ -50,3 +52,48 @@ class ReplicatorTest(TestCase):
         # Then
         self.assertEqual(replicator.tables, ["account", 'testbase'])
 
+    def test_main_init_should_contain_an_array_of_databases_with_one_element(self):
+        # Given
+        config_parser = ConfigParser()
+        config_parser['mysql'] = {
+            'host' : 'test_host',
+            'port' : '1111',
+            'user' : 'user',
+            'password' : 'password',
+            'databases' : 'db',
+            'server_id' : '1',
+            'tables' : 'account',
+            'index_label' : 'id'
+        }
+        config_parser['core'] = {
+            'modules_path' : 'path/to/modules'
+        }
+        # When
+        # When
+        replicator = Replicator(config_parser)
+
+        # Then
+        self.assertEqual(replicator.databases, ["db"])
+
+    def test_main_init_should_contain_an_array_of_databases_with_two_elements(self):
+        # Given
+        config_parser = ConfigParser()
+        config_parser['mysql'] = {
+            'host' : 'test_host',
+            'port' : '1111',
+            'user' : 'user',
+            'password' : 'password',
+            'databases' : 'db, db2',
+            'server_id' : '1',
+            'tables' : 'account',
+            'index_label' : 'id'
+        }
+        config_parser['core'] = {
+            'modules_path' : 'path/to/modules'
+        }
+        # When
+        # When
+        replicator = Replicator(config_parser)
+
+        # Then
+        self.assertEqual(replicator.databases, ["db", "db2"])
